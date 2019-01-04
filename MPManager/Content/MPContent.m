74825b170ecfa8186257ff4dff0c97b482d8c7fb
Declan Land <thejailbreaker@me.com>
2019-01-04 11:20:27 +0100

    Update MPContent.m

--- MPManager/Content/MPContent.m
+++ MPManager/Content/MPContent.m
@@ -10,4 +10,52 @@
 
 @implementation MPContent
 
+- (id)init {
+    self = [super init];
+    if (self) {
+        [self setup];
+    } return self;
+}
+
+#pragma mark    -   Setup:
+
+- (void)setup {
+    
+    
+    
+}
+
+#pragma mark    -   Checker:
+
+- (BOOL)hasValidContent {
+    
+    if (!self.image && !self.string && !self.htmlString && !self.mutableString && !self.view && !self.url) {
+        return NO;
+    }
+    
+    //  check string length:
+    if (self.string) {
+        if (self.string.length == 0) {
+            return NO;
+        }
+    }
+    
+    //  check html string length:
+    if (self.htmlString) {
+        if (self.htmlString.length == 0) {
+            return NO;
+        }
+    }
+    
+    //  check mutable string length:
+    if (self.mutableString) {
+        if (self.mutableString.length == 0) {
+            return NO;
+        }
+    }
+    
+    return YES;
+    
+}
+
 @end
